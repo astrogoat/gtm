5d24911ad7de7ca2c60d7be1790a82215935dce3
Brett M <brett@helixsleep.com>
2022-01-27 15:04:31 -0500

    Removed bad migration updated new migration

--- database/migrations/settings/2021_09_30_100000_create_gtm_settings.php
+++ database/migrations/settings/2021_09_30_100000_create_gtm_settings.php
@@ -7,14 +7,12 @@ class CreateGtmSettings extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('gtm.enabled', false);
-        // $this->migrator->add('gtm.url', '');
-        // $this->migrator->addEncrypted('gtm.access_token', '');
+        $this->migrator->add('gtm.container_id', '');
     }
 
     public function down()
     {
         $this->migrator->delete('gtm.enabled');
-        // $this->migrator->delete('gtm.url');
-        // $this->migrator->delete('gtm.access_token');
+        $this->migrator->delete('gtm.container_id');
     }
 }

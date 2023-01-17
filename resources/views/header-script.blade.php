82add4eddaddc756bbe4fc58e793a4505b2a6670
kofi amoussou <karsoft92@gmail.com>
2023-01-17 20:52:38 +0000

    Add server side url feature to header script

--- resources/views/header-script.blade.php
+++ resources/views/header-script.blade.php
@@ -1,4 +1,11 @@
 @if(\Astrogoat\Gtm\Settings\GtmSettings::isEnabled())
+    @php
+        $server_side_url = settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
+
+        if(blank($server_side_url)){
+            $server_side_url = 'googletagmanager.com';
+        }
+    @endphp
     <!-- [GTM] Header | Start -->
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -16,7 +23,7 @@
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://{{ $server_side_url }}/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','{{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}');
     </script>
     <!-- [GTM] Header | End -->

c679942dacbd52e60b698d58a7415db685de1ee9
kofi amoussou <karsoft92@gmail.com>
2023-01-19 12:17:49 +0000

    Add validation to remove duplicate https

--- resources/views/body-script.blade.php
+++ resources/views/body-script.blade.php
@@ -1,12 +1,21 @@
 @if(\Astrogoat\Gtm\Settings\GtmSettings::isEnabled())
+
     @php
 
         $server_side_url = settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
 
         if(blank($server_side_url)){
-            $server_side_url = 'googletagmanager.com';
+
+            $server_side_url = 'www.googletagmanager.com';
+
+        }else{
+
+            $server_side_url = Str::of($server_side_url,'https://')
+                                ->replaceFirst(url('https://'), '');
         }
+
     @endphp
+
     <!-- [GTM] Body | Start -->
     <noscript><iframe src="https://{{ $server_side_url }}/ns.html?id={{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
--- resources/views/header-script.blade.php
+++ resources/views/header-script.blade.php
@@ -1,10 +1,18 @@
 @if(\Astrogoat\Gtm\Settings\GtmSettings::isEnabled())
     @php
+
         $server_side_url = settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
 
         if(blank($server_side_url)){
-            $server_side_url = 'googletagmanager.com';
+
+            $server_side_url = 'www.googletagmanager.com';
+
+        }else{
+
+            $server_side_url = Str::of($server_side_url,'https://')
+                                ->replaceFirst(url('https://'), '');
         }
+
     @endphp
     <!-- [GTM] Header | Start -->
     <script>

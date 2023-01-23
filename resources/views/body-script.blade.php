174b6d10f4aff10d61ba2b87983a10e5da3bb956
kofi amoussou <karsoft92@gmail.com>
2023-01-23 13:14:06 +0000

    Update scripts and variables

--- resources/views/body-script.blade.php
+++ resources/views/body-script.blade.php
@@ -5,7 +5,7 @@
             : settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
     @endphp
     <!-- [GTM] Body | Start -->
-    <noscript><iframe src="{{ $server_side_url }}/ns.html?id={{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}"
+    <noscript><iframe src="{{ $serverSideUrl }}/ns.html?id={{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- [GTM] End | Start -->
 @endif
--- resources/views/header-script.blade.php
+++ resources/views/header-script.blade.php
@@ -1,19 +1,8 @@
 @if(\Astrogoat\Gtm\Settings\GtmSettings::isEnabled())
     @php
-
-        $server_side_url = settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
-
-        if(blank($server_side_url)){
-
-            $server_side_url = 'https://www.googletagmanager.com';
-
-        }else{
-
-          if(! Str::startsWith($server_side_url,'https://')){
-                $server_side_url = Str::start($server_side_url,'https://');
-            }
-        }
-
+      $serverSideUrl = blank(settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url'))
+            ? 'https://www.googletagmanager.com'
+            : settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
     @endphp
     <!-- [GTM] Header | Start -->
     <script>
@@ -32,7 +21,7 @@
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            '{{ $server_side_url }}/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            '{{ $serverSideUrl }}/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','{{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}');
     </script>
     <!-- [GTM] Header | End -->

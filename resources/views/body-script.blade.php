6f0ee38e60a8713c7ce6f7d195a115f384dc81b7
Kofi Ramos <karsoft92@gmail.com>
2023-01-23 13:08:08 +0000

    Update resources/views/body-script.blade.php
    Co-authored-by: Kristoffer Tonning <github@tonning.dev>

--- resources/views/body-script.blade.php
+++ resources/views/body-script.blade.php
@@ -1,24 +1,9 @@
 @if(\Astrogoat\Gtm\Settings\GtmSettings::isEnabled())
-
     @php
-
-        $server_side_url = settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
-
-        if(blank($server_side_url)){
-
-            $server_side_url = 'https://www.googletagmanager.com';
-
-        }else{
-
-            if(! Str::startsWith($server_side_url,'https://')){
-                $server_side_url = Str::start($server_side_url,'https://');
-
-            }
-
-        }
-
+        $serverSideUrl = blank(settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url'))
+            ? 'https://www.googletagmanager.com'
+            : settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'server_side_url');
     @endphp
-
     <!-- [GTM] Body | Start -->
     <noscript><iframe src="{{ $server_side_url }}/ns.html?id={{ settings(Astrogoat\Gtm\Settings\GtmSettings::class, 'container_id') }}"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
